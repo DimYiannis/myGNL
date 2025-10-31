ee18297895b5ba41ab265c0b448769bd9607fe38
DimYiannis <johndim99@gmail.com>
2025-10-31 22:05:31 +0200

    populating stash , checking if we meet newline char and copy to line

--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,18 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 11:29:16 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/31 15:33:46 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/31 21:22:34 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
 
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
 
 #endif
