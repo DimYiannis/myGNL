8da70afeb32a5a140ea79a93b99926966202aa24
DimYiannis <johndim99@gmail.com>
2025-10-31 22:32:45 +0200

    pointers to keep track where i was left in stash so i can keep copying to line

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 11:26:51 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/31 22:04:52 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/31 22:30:40 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,12 @@ char *get_next_line(int fd)
     char *buffer;
     int res;
     static char *stash;
+    static char *stashwrite;
     char *line;
     int i;
+    char *stash_start;
+    char *line_start;
     
-
     fd = open("text.txt", O_RDWR);
     if (fd == -1 || BUFFER_SIZE <= 0)
         return (NULL);
@@ -38,20 +40,34 @@ char *get_next_line(int fd)
             free(buffer);
             return NULL;
         }
-        
+        stash[0] = '\0';
+        stashwrite = stash;
     }
+    stash_start = stash;
+    line_start = line;
+    
     i = 0;
     //populate stash
     while ((res = read(fd, buffer, 5)) > 0)
     {
         while (i++ < res)
-            *stash++ = buffer[i];
+            *stashwrite++ = buffer[i];
     }
-    *stash = '\0';
+    *stashwrite = '\0';
     free(buffer);
-    stash = stash - i;
+
     // check for newline and copy to line
-    while (stash != '\n' && stash)
-        *line++ = *stash++;
+    line = malloc(i + 1);
+    while (stash_start != '\n' && stash_start)
+        *line++ = *stash_start++;
     *line = '\0';
+    
+    if (*stash_start == '\n')
+        stash_start++;
+    stashwrite = stash;
+    while(*stashwrite)
+        *stashwrite = *stash_start;
+    *stashwrite = '\0';
+    
+    return (line_start);
 }
