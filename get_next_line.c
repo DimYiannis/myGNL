4555c6613fff54b56f098df5ba75a5e9ec2df5ac
Yiannis Dimitrakopoulos <ydimitra@f1r4s2.codam.nl>
2025-10-31 15:46:25 +0100

    declared initial vars and use open to access the txt file and read to get the data from the file

--- get_next_line.c
+++ get_next_line.c
@@ -1,17 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   gte_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 11:26:51 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/31 11:27:04 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/10/31 15:34:02 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
 char *get_next_line(int fd)
 {
+    int fd;
+    char buffer[100];
+    int res;
+    
+
+    fd = open("text.txt", O_RDWR);
+    if (fd == -1)
+        return (NULL); 
+    res = read(fd, buffer, 5);
+    if (res != 5)
+        return (NULL);
     
 }
