2c7c83b216a7aaa45060f702d48141489dcf6e70
yiannis <107484245+DimYiannis@users.noreply.github.com>
2025-11-01 16:52:58 +0100

    structure of initial logic havent used any of the helper funcs

--- get_next_line.h
+++ get_next_line.h
@@ -6,19 +6,23 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 11:29:16 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/11/01 13:57:31 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/11/01 14:46:34 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
+# include <fcntl.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s1);
 
 #endif
