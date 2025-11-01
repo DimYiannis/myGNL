2188841242e8aa6f983b4d81f7110e0e88f4ecd6
yiannis <107484245+DimYiannis@users.noreply.github.com>
2025-11-01 15:31:25 +0100

    helper functions

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,9 +6,54 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 11:27:26 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/10/31 15:34:14 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/11/01 14:46:17 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned char	ch;
+
+	ch = (unsigned char)c;
+	while (*s)
+	{
+		if (*s == ch)
+			return ((char *)s);
+		s++;
+	}
+	if (ch == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	len;
+
+	len = 0;
+	while (s[len])
+	{
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	len;
+	char	*s2;
+
+	len = 0;
+	while (s1[len])
+		len++;
+	s2 = (char *)malloc(sizeof(char) * (len + 1));
+	if (!s2)
+		return (NULL);
+	while (*s1)
+		*s2++ = *s1++;
+	*s2 = '\0';
+	s2 = s2 - len;
+	return (s2);
+}
