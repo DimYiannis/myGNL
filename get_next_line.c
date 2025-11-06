4f4985dde6f7ff94e99c9d7d21f8e120471d1ce5
yiannis <107484245+DimYiannis@users.noreply.github.com>
2025-11-06 11:51:58 +0100

    norm

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ydimitra <ydimitra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 11:26:51 by ydimitra          #+#    #+#             */
-/*   Updated: 2025/11/05 23:58:10 by ydimitra         ###   ########.fr       */
+/*   Updated: 2025/11/06 11:51:17 by ydimitra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,30 +26,30 @@ static char	*no_stash(char *s)
 
 static char	*ft_join(char *s1, char *s2)
 {
-  char *string;
-  int len1;
-  int len2;
-  int i;
-  int j;
+	char	*string;
+	int		len1;
+	int		len2;
+	int		i;
+	int		j;
 
-  i = 0;
-  j = 0;
-  s1 = no_stash(s1);
-  len1 = ft_strlen(s1);
-  len2 = ft_strlen(s2);
-  string = malloc(len1 + len2 + 1);
-  if (!string)
-    return (NULL);
-  while (s1[i])
-  {
-    string[i] = s1[i];
-    i++;
-  }
-  while (s2[j])
-    string[i++] = s2[j++];
-  string[i] = '\0';
-  free(s1);
-  return (string);
+	i = 0;
+	j = 0;
+	s1 = no_stash(s1);
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	string = malloc(len1 + len2 + 1);
+	if (!string)
+		return (NULL);
+	while (s1[i])
+	{
+		string[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+		string[i++] = s2[j++];
+	string[i] = '\0';
+	free(s1);
+	return (string);
 }
 
 static char	*extract_line(char **stash)
@@ -68,11 +68,11 @@ static char	*extract_line(char **stash)
 	else
 		line = ft_substr(*stash, 0, i);
 	if ((*stash)[i] == '\n')
-  {
-    new_stash = ft_strdup(*stash + i + 1);
-    if (!new_stash)
-      return (free(*stash), *stash = NULL, line);
-  }
+	{
+		new_stash = ft_strdup(*stash + i + 1);
+		if (!new_stash)
+			return (free(*stash), *stash = NULL, line);
+	}
 	else
 		new_stash = NULL;
 	free(*stash);
@@ -82,30 +82,27 @@ static char	*extract_line(char **stash)
 
 char	*get_next_line(int fd)
 {
-  char	*buffer;
-	ssize_t bytes;
+	char		*buffer;
+	ssize_t		bytes;
 	static char	*stash;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-  stash = no_stash(stash);
-  buffer = malloc(BUFFER_SIZE + 1);
-  if (!buffer)
-    return (NULL);
+	stash = no_stash(stash);
+	buffer = malloc(BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
 	bytes = 1;
-  while (bytes > 0 && !ft_strchr(stash, '\n'))
-  {
-    bytes = read(fd, buffer, BUFFER_SIZE);
-    if (bytes <= 0)
-      return(free(stash), free(buffer), stash = NULL, NULL);
-    buffer[bytes] = '\0';
-    stash = ft_join(stash, buffer);
-    if (!stash)
-    {
-      free(buffer);
-      return (NULL);
-    }
-  }
-  free(buffer);
+	while (bytes > 0 && !ft_strchr(stash, '\n'))
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		if (bytes <= 0)
+			return (free(stash), free(buffer), stash = NULL, NULL);
+		buffer[bytes] = '\0';
+		stash = ft_join(stash, buffer);
+		if (!stash)
+			return (free(buffer), NULL);
+	}
+	free(buffer);
 	return (extract_line(&stash));
 }
